c23a91398d962889858ba71978f5936213f4aba4
Igor Trofimov <igorecus@yandex.ru>
2025-06-22 18:56:03 +0300

    Тесты пройдены. Сделан Allure-отчёт

--- urls.py
+++ urls.py
@@ -2,9 +2,4 @@ class Urls:
     BASE_URL = "https://stellarburgers.nomoreparties.site"
     LOGIN_PAGE = f"{BASE_URL}/login"
     MAIN_PAGE = BASE_URL
-    FEED_PAGE = f"{BASE_URL}/feed"
     FORGOT_PASSWORD_PAGE = f"{BASE_URL}/forgot-password"
-    RESET_PASSWORD_PAGE = f"{BASE_URL}/reset-password"
-    ACCOUNT_PAGE = f"{BASE_URL}/account/profile"
-    ORDER_HISTORY_PAGE = f"{BASE_URL}/account/order-history"
-    INGREDIENT_DETAILS_PAGE = f"{BASE_URL}/ingredient/61c0c5a71d1f82001bdaaa6d"
